e982b85f19046332c942020db2688244b846189a
HZ-njupt <1553780451@qq.com>
2020-02-25 19:45:25 +0800

    hz-02.25

---
+++ src/components/login.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script>
+   import mock from '@/mock/index.js';
+   import Cookies from 'js-cookies';
+   import router from '@router';
+   export default{
+       
+   }
+</script>
